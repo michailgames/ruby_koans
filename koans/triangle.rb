ed6bf20a63fe0c498f1e1a037566f9758bc9550d
michail <michailgames@yahoo.pl>
2014-10-12 15:51:42 +0200

    triangles

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b
+  	b == c ? :equilateral : :isosceles
+  else
+  	(b == c or a == c) ? :isosceles : :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
